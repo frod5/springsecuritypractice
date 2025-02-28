4ac925038dd56867a79c80f006027d7ce9c7984a
김선우 <frod5@kakao.com>
2025-03-01 00:01:12 +0900

    Spring security 실습 프로젝트 - 커스텀 logout

---
+++ src/main/java/io/security/springsecuritymaster/security/provider/FormAuthenticationProvider.java
@@ -0,0 +1,43 @@
+package io.security.springsecuritymaster.security.provider;
+
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import io.security.springsecuritymaster.security.service.UserDetailService;
+
+@Component("authenticationProvider")
+public class FormAuthenticationProvider implements AuthenticationProvider {
+
+	private final UserDetailService userDetailService;
+	private final PasswordEncoder passwordEncoder;
+
+	public FormAuthenticationProvider(PasswordEncoder passwordEncoder, UserDetailService userDetailService) {
+		this.passwordEncoder = passwordEncoder;
+		this.userDetailService = userDetailService;
+	}
+
+	@Override
+	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+		String username = authentication.getName();
+		String password = (String) authentication.getCredentials();
+
+		UserDetails userDetails = userDetailService.loadUserByUsername(username);
+
+		if (!passwordEncoder.matches(password, userDetails.getPassword())) {
+			throw new BadCredentialsException("Invalid password");
+		}
+
+		return new UsernamePasswordAuthenticationToken(userDetails, password, userDetails.getAuthorities());
+	}
+
+	@Override
+	public boolean supports(Class<?> authentication) {
+		return authentication.isAssignableFrom(UsernamePasswordAuthenticationToken.class);
+	}
+}
--- src/main/java/io/security/springsecuritymaster/users/controller/LoginController.java
+++ src/main/java/io/security/springsecuritymaster/users/controller/LoginController.java
@@ -1,7 +1,13 @@
 package io.security.springsecuritymaster.users.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
 public class LoginController {
@@ -14,5 +20,17 @@ public class LoginController {
 	@GetMapping("/sign-up")
 	public String signUp() {
 		return "login/signUp";
+	}
+
+	@GetMapping("/logout")
+	public String logout(HttpServletRequest request, HttpServletResponse response) {
+		Authentication auth = SecurityContextHolder.getContextHolderStrategy()
+			.getContext()
+			.getAuthentication();
+		if (auth != null) {
+			new SecurityContextLogoutHandler().logout(request, response, auth);
+		}
+
+		return "redirect:/login";
 	}
 }
