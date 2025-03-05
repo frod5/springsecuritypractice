9432c033f89eeec4a6ef177b52928dd956ded8ca
김선우 <frod5@kakao.com>
2025-03-05 17:18:29 +0900

    Spring security 실습 프로젝트 - rest Custom Filter 추가

--- src/main/java/io/security/springsecuritymaster/security/config/SecurityConfig.java
+++ src/main/java/io/security/springsecuritymaster/security/config/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationDetailsSource;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -12,8 +14,10 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.WebAuthenticationDetails;
 
+import io.security.springsecuritymaster.security.filter.RestAuthenticationFilter;
 import io.security.springsecuritymaster.security.handler.FormAccessDeniedHandler;
 import io.security.springsecuritymaster.security.handler.FormAuthenticationSuccessHandler;
 import jakarta.servlet.http.HttpServletRequest;
@@ -59,15 +63,27 @@ public class SecurityConfig {
 	@Bean
 	@Order(1)
 	public SecurityFilterChain restSecurityFilterChain(HttpSecurity http) throws Exception {
+
+		AuthenticationManagerBuilder builder = http.getSharedObject(AuthenticationManagerBuilder.class);
+		AuthenticationManager manager = builder.build();
+
 		http.securityMatcher("/api/**")
 			.authorizeHttpRequests(auth -> auth
 				.requestMatchers("/css/**", "/images/**", "/js/**", "/favicon.*", "/*/icon-*", "/h2-console/**")
 				.permitAll()
 				.anyRequest()
 				.permitAll()
-			).csrf(AbstractHttpConfigurer::disable);
+			).csrf(AbstractHttpConfigurer::disable)
+			.addFilterBefore(restAuthenticationFilter(manager), UsernamePasswordAuthenticationFilter.class)
+			.authenticationManager(manager);
 
 		return http.build();
+	}
+
+	private RestAuthenticationFilter restAuthenticationFilter(AuthenticationManager manager) {
+		RestAuthenticationFilter filter = new RestAuthenticationFilter();
+		filter.setAuthenticationManager(manager);
+		return filter;
 	}
 
 	@Bean
